baa352f33a655595c914fd6ef7f83f47d198e8ec
Jag <jagdeepsingh3939@gmail.com>
2019-06-18 21:40:07 -0700

    removed styleguide build from gitignore

--- README.md
+++ README.md
@@ -1,30 +1,32 @@
-![CF](http://i.imgur.com/7v5ASc8.png) LAB 27
-=================================================
+# ![CF](http://i.imgur.com/7v5ASc8.png) LAB 27
 
 ## React Testing and Deployment Style Guide
 
 ### Author: Jagdeep Singh
 
 ### Links and Resources
-* [submission PR](https://github.com/401-advanced-javascript-js/lab-27-react-testing-and-deployment/pull/1)
-* [travis](https://www.travis-ci.com/401-advanced-javascript-js/lab-27-react-testing-and-deployment)
-* [front-end](https://submission.d2hpveud6ur4wa.amplifyapp.com/)
+
+- [submission PR](https://github.com/401-advanced-javascript-js/lab-27-react-testing-and-deployment/pull/1)
+- [travis](https://www.travis-ci.com/401-advanced-javascript-js/lab-27-react-testing-and-deployment)
+- [front-end](https://submission.d2hpveud6ur4wa.amplifyapp.com/)
 
 #### Documentation
+
 <!-- * [jsdoc](http://xyz.com) (Server assignments) -->
-* [styleguide](./styleguide/build/index.html)
+
+- [styleguide](./styleguide/build/index.html)
 
 #### Running the app
-* `npm start`
-  
+
+- `npm start`
+
 #### Tests
-* How do you run tests?   
-  
+
+- How do you run tests?
+
   `npm test`
 
-* What assertions were made? 
-    
-    tests for counter component
+- What assertions were made? tests for counter component
 
 <!-- #### UML
 ![UML]() -->
---
+++ styleguide/build/1.0d35d89c.js
@@ -0,0 +1,25 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([[1],{
+
+/***/ "./node_modules/css-loader/index.js?!./node_modules/postcss-loader/src/index.js?!./node_modules/sass-loader/lib/loader.js!./src/components/counter/counter.scss":
+/*!*****************************************************************************************************************************************************************************!*\
+  !*** ./node_modules/css-loader??ref--6-oneOf-5-1!./node_modules/postcss-loader/src??postcss!./node_modules/sass-loader/lib/loader.js!./src/components/counter/counter.scss ***!
+  \*****************************************************************************************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+eval("exports = module.exports = __webpack_require__(/*! ../../../node_modules/css-loader/lib/css-base.js */ \"./node_modules/css-loader/lib/css-base.js\")(false);\n// imports\n\n\n// module\nexports.push([module.i, \".counter {\\n  text-align: center;\\n  margin: 75px;\\n  background-color: #716c86;\\n  padding: 30px;\\n  min-width: 400px;\\n  border-radius: 1em; }\\n  .counter > span {\\n    font-size: 50pt;\\n    color: #FF8811; }\\n  .counter > .clicker {\\n    font-size: 25pt;\\n    text-decoration: none;\\n    color: #FFEAEE;\\n    margin: 20px auto;\\n    padding: 25px;\\n    border-radius: 5px; }\\n  .counter .positive {\\n    color: lightgreen; }\\n  .counter .negative {\\n    color: #fd5858; }\\n\", \"\"]);\n\n// exports\n\n\n//# sourceURL=webpack:///./src/components/counter/counter.scss?./node_modules/css-loader??ref--6-oneOf-5-1!./node_modules/postcss-loader/src??postcss!./node_modules/sass-loader/lib/loader.js");
+
+/***/ }),
+
+/***/ "./src/components/counter/counter.scss":
+/*!*********************************************!*\
+  !*** ./src/components/counter/counter.scss ***!
+  \*********************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+eval("\nvar content = __webpack_require__(/*! !../../../node_modules/css-loader??ref--6-oneOf-5-1!../../../node_modules/postcss-loader/src??postcss!../../../node_modules/sass-loader/lib/loader.js!./counter.scss */ \"./node_modules/css-loader/index.js?!./node_modules/postcss-loader/src/index.js?!./node_modules/sass-loader/lib/loader.js!./src/components/counter/counter.scss\");\n\nif(typeof content === 'string') content = [[module.i, content, '']];\n\nvar transform;\nvar insertInto;\n\n\n\nvar options = {\"hmr\":true}\n\noptions.transform = transform\noptions.insertInto = undefined;\n\nvar update = __webpack_require__(/*! ../../../node_modules/style-loader/lib/addStyles.js */ \"./node_modules/style-loader/lib/addStyles.js\")(content, options);\n\nif(content.locals) module.exports = content.locals;\n\nif(false) {}\n\n//# sourceURL=webpack:///./src/components/counter/counter.scss?");
+
+/***/ })
+
+}]);
